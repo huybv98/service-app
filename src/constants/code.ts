3b6ed64f3addf3504b000bf7f348af7ecc186171
Huybv98 <mcnb.1998@gmail.com>
2023-04-05 11:51:28 +0700

    update api auth

---
+++ src/constants/code.ts
@@ -0,0 +1,5 @@
+const codeApi = {
+  CODE_OK: 'API000',
+  CODE_FAIL: 'API001',
+}
+export default codeApi
---
+++ src/constants/message.ts
@@ -0,0 +1,5 @@
+const messageApi = {
+  CODE_OK: 'SUCCESS',
+  CODE_FAIL: 'FAIL',
+}
+export default messageApi
